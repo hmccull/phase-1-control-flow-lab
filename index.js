58d01beb8374353f3ca6189111d20164b56ad9e6
Hannah McCullough <hmac091@gmail.com>
2021-11-22 16:37:37 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(numOfFeet){
+  // create result var
+  let greeting = '';
+
+  if (numOfFeet < 200) {
+    greeting = 'This one is on me!';
+  } else if (numOfFeet > 2000 && numOfFeet < 2500) {
+    greeting = 'I will gladly take your thirty bucks.';
+  } else {
+    greeting = 'No can do.';
+  }
+
+  // return result
+  return greeting;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  // create result var
+  let message = '';
+
+  city === 'NYC' ? message = 'Ok, sounds good.' : message = 'No go.';
+
+  // return result var
+  return message;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  // create result var
+  let message = '';
+
+  switch(tip) {
+    case 'generous': message = 'Thank you so much.';
+    break;
+    case 'not as generous': message = 'Thank you.';
+    break;
+    default: message = 'Bye.';
+  }
+
+  // return result var
+  return message;
 }
